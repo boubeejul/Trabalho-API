c87c4ecf16955f4353a6f4804966646681cebe9d
boubeejul <juliana.boubee@gmail.com>
2023-06-13 13:56:25 -0300

    atualização de permissão

--- src/main/java/com/trabalhoFinal/apiEcommerce/security/WebSecurityConfig.java
+++ src/main/java/com/trabalhoFinal/apiEcommerce/security/WebSecurityConfig.java
@@ -42,8 +42,8 @@ public class WebSecurityConfig {
             .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandler)) //configura a classe para tratamento da excecao de autenticacao
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //define a politica de sessao
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth/**", "/roles", "/produtos/dto", "/categorias/dto", "/swagger-ui/**", "/v3/api-docs/**", "/upload/**",  "/produtos/**", "/categorias/**").permitAll() //define as rotas publicas/abertas
-                    .requestMatchers("/clientes/**", "/enderecos/**", "/itemPedidos/**", "/pedidos/**", "/users/**").hasAnyRole("ADMIN", "MODERATOR", "USER") //autoriza o acesso a rotas por perfis
+                    .requestMatchers("/auth/**", "/roles", "/produtos/dto", "/categorias/dto", "/swagger-ui/**", "/v3/api-docs/**", "/upload/**",  "/produtos/**", "/categorias/**", "/clientes/**").permitAll() //define as rotas publicas/abertas
+                    .requestMatchers("/enderecos/**", "/itemPedidos/**", "/pedidos/**", "/users/**").hasAnyRole("ADMIN", "MODERATOR", "USER") //autoriza o acesso a rotas por perfis
                     .anyRequest().authenticated()) //demais rotas, nao configuradas acima, so poderao ser acessadas mediante autenticacao
 		;		
 		
